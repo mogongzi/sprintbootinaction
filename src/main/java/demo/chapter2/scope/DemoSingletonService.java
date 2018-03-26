65fdd3d6d4c89d15dba08265b89373f7647d0119
ryan.ren <mogongzi@sina.cn>
2018-03-26 22:23:35 +0800

    add demo

---
+++ src/main/java/demo/chapter2/scope/DemoSingletonService.java
@@ -0,0 +1,7 @@
+package demo.chapter2.scope;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSingletonService {
+}
---
+++ src/main/java/demo/chapter2/scope/ScopeConfig.java
@@ -0,0 +1,9 @@
+package demo.chapter2.scope;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("demo.chapter2.scope")
+public class ScopeConfig {
+}
