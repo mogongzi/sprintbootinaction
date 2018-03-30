6b3e9afb3825d7a80525bcb850200269dbfc2d46
ryan.ren <mogongzi@sina.cn>
2018-03-30 23:53:04 +0800

    finish chatper 6

---
+++ springboot2/src/main/java/demo/spingaction/springboothello/App.java
@@ -0,0 +1,13 @@
+package demo.spingaction.springboothello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ springboot2hellodemo/src/main/java/demo/spinaction/springboot2/hellodemo/hellodemo/HellodemoApplication.java
@@ -0,0 +1,25 @@
+package demo.spinaction.springboot2.hellodemo.hellodemo;
+
+import demo.spingaction.springboothello.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HellodemoApplication {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping("/")
+    public String index() {
+        return helloService.sayHello();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellodemoApplication.class, args);
+    }
+}
