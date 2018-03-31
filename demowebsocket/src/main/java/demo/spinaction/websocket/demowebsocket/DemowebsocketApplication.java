8d769c6366d1d1a50715f6c66e2492cbef1f46d5
ryan.ren <mogongzi@sina.cn>
2018-03-31 23:13:46 +0800

    finish websocket demo

---
+++ demowebsocket/src/main/java/demo/spinaction/websocket/demowebsocket/DemowebsocketApplication.java
@@ -0,0 +1,12 @@
+package demo.spinaction.websocket.demowebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemowebsocketApplication.class, args);
+    }
+}
---
+++ demowebsocket/src/main/java/demo/spinaction/websocket/demowebsocket/domain/DemoMessage.java
@@ -0,0 +1,10 @@
+package demo.spinaction.websocket.demowebsocket.domain;
+
+public class DemoMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
