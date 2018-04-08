8aeba5f39687d66e63d466de2fd3d5d0802ce4e3
ryan.ren <mogongzi@sina.cn>
2018-04-08 22:43:34 +0800

    fix static/css blocking by spring security issue

--- demosecurity/src/main/java/demo/springboot/security/config/WebMvcConfig.java
+++ demosecurity/src/main/java/demo/springboot/security/config/WebMvcConfig.java
@@ -1,22 +1,14 @@
 package demo.springboot.security.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
     }
 }
