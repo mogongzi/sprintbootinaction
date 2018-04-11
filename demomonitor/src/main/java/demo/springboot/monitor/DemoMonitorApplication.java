fad154b57716fd10e12f7364d98f1164aec7418c
Ryan Ren <ryan.ren@blackboard.com>
2018-04-11 15:18:07 +0800

    add spring boot 2 monitor demo

---
+++ demomonitor/src/main/java/demo/springboot/monitor/DemoMonitorApplication.java
@@ -0,0 +1,12 @@
+package demo.springboot.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMonitorApplication.class, args);
+	}
+}
